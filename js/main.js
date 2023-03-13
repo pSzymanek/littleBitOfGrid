8fed408b19e5549a97d0ec0a28a1ee0aec1ebc21
pSzymanek <poczta.piotrszymanek@gmail.com>
2023-03-13 14:48:21 +0100

    another sections, final version

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
-
-
+const allNavItems = document.querySelectorAll('.nav__link')
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active');
@@ -11,10 +10,13 @@ const handleNav = () => {
 }
 navBtn.addEventListener('click', handleNav)
 
-
-
 const handleCurrentYear = () => {
     const year = (new Date).getFullYear();
     footerYear.innerText = year;
 }
 handleCurrentYear()
+
+
+
+allNavItems.forEach(item => item.addEventListener('click', () => navMobile.classList.remove('nav-mobile--active')))
+allNavItems.forEach(item => item.addEventListener('click', () => navBtn.classList.remove('is-active')))
